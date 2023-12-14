4ad9867f92826ee561258302c8cc3ffc1316aeb8
shamilkvv <shamiltv803@gmail.com>
2023-12-14 14:18:46 +0400

    Добавьте файлы проекта.

---
+++ list/list.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+template<typename T>
+class List
+{
+private:
+    struct Node
+    {
+        T element;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+
+    };
+
+    Node* first = nullptr;
+    Node* last = nullptr;
+
+    int elementsCount = 0;
+
+};
+
+int main()
+{
+    
+
+    return 0;
+}
